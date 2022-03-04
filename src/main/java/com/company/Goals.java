3dd976dce30f7865d8f59bcd4243ecf7e93bf7c7
Your Name <you@example.com>
2022-03-03 16:43:42 -0800

    Innitial commit

---
+++ src/main/java/com/company/Goals.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Goals {
+    public static void main(String[] args) {
+
+        System.out.println("My goals for this class are: To become a software developer, to get and internship " +
+                "this summer and lastly be able to get a job." );
+    }
+}
